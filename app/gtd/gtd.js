9a58e2de1b33494ed98588c5e9d1b099dc3631e9
Brian Wigginton <brianwigginton@gmail.com>
2015-03-31 22:00:33 -0500

    jshint fixed for gtd file

--- app/gtd/gtd.js
+++ app/gtd/gtd.js
@@ -1,39 +1,37 @@
 (function gtd(_, $, rivets) {
+  'use strict';
 
-    var body = document.body;
+  var body = document.body;
 
-    // handle checkboxes for tasks
-    body.innerHTML = body.innerHTML
-        // action
-        .replace(/\[\](.+)\s@action/gi, '<gtd class="gtd-action"><span class="fa fa-square-o"></span> $1</gtd>')
-        // later
-        .replace(/\[\](.+)\s@later/gi, '<gtd class="gtd-later"><span class="fa fa-calendar"></span> $1</gtd>')
-        // done
-        .replace(/\[\](.+)\s@done/gi, '<gtd class="gtd-done"><span class="fa fa-check-square"></span> <del>$1</del></gtd>')
-        .replace(/\[[x\/]\](.+)\n/gi, '<gtd class="gtd-done"><span class="fa fa-check-square"></span> <del>$1</del></gtd>')
-        // inbox
-        .replace(/\[\](.+)\n/gi,  '<gtd class="gtd-inbox"><span class="fa fa-square-o"></span> $1</gtd>')
+  // handle checkboxes for tasks
+  body.innerHTML = body.innerHTML
+    // action
+    .replace(/\[\](.+)\s@action/gi, '<gtd class="gtd-action"><span class="fa fa-square-o"></span> $1</gtd>')
+    // later
+    .replace(/\[\](.+)\s@later/gi, '<gtd class="gtd-later"><span class="fa fa-calendar"></span> $1</gtd>')
+    // done
+    .replace(/\[\](.+)\s@done/gi, '<gtd class="gtd-done"><span class="fa fa-check-square"></span> <del>$1</del></gtd>')
+    .replace(/\[[x\/]\](.+)\n/gi, '<gtd class="gtd-done"><span class="fa fa-check-square"></span> <del>$1</del></gtd>')
+    // inbox
+    .replace(/\[\](.+)\n/gi,  '<gtd class="gtd-inbox"><span class="fa fa-square-o"></span> $1</gtd>');
 
-    if(!_.isUndefined(GTD_NOTES)) {
+  $('gtd-notes').show();
 
-        $('gtd-notes').show();
+  var tasks = [];
 
-        tasks = [];
+  _.each($('gtd'), function(item) {
+    tasks.push({
+      name: 'sacdas',
+      state: item.className.split('-')[1]
+    });
+  });
 
-        _.each($('gtd'), function(item) {
-            tasks.push({
-                name: 'sacdas',
-                state: item.className.split('-')[1]
-            });
-        })
-
-        rivets.bind($('gtd-notes'), {
-          action: _.where(tasks, {state: 'action'}),
-          done: _.where(tasks, {state: 'done'}),
-          inbox: _.where(tasks, {state: 'inbox'}),
-          later: _.where(tasks, {state: 'later'}),
-          tasks: tasks
-        })
-    }
+  rivets.bind($('gtd-notes'), {
+    action: _.where(tasks, {state: 'action'}),
+    done: _.where(tasks, {state: 'done'}),
+    inbox: _.where(tasks, {state: 'inbox'}),
+    later: _.where(tasks, {state: 'later'}),
+    tasks: tasks
+  });
 
 })(_, jQuery, rivets);
